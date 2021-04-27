61879db3125a83de369358f38bbc376c2ecee315
hamzamughal <mughalh473@gmail.com>
2021-04-27 14:36:16 +0500

    database connection

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+// Connect to DB
+const url = 'mongodb+srv://node-auth:node-auth@cluster0.bdva6.mongodb.net/auth?retryWrites=true&w=majority'
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, () => console.log(`connected to DB`),)
+
 // import routes
 const authRoute = require('./routes/auth')
 
 // route middleware
 app.use('/api/user', authRoute)
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log(`Server Up and Running..!!`);
 })
