8d53f8a9699c5efbc52f59cb33bd4895413bb0e3
hamzamughal <mughalh473@gmail.com>
2021-04-27 14:40:26 +0500

    database connection added to env

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
+dotenv.config()
+
 // Connect to DB
-const url = 'mongodb+srv://node-auth:node-auth@cluster0.bdva6.mongodb.net/auth?retryWrites=true&w=majority'
+const url = process.env.DB_CONNECT
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, () => console.log(`connected to DB`),)
 
 // import routes
