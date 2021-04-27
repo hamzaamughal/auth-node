639085fd566e819812a1574cedd5e17138a8c989
hamzamughal <mughalh473@gmail.com>
2021-04-27 14:08:05 +0500

    routes added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
-
 const app = express()
+// import routes
+const authRoute = require('./routes/auth')
+
+// route middleware
+app.use('/api/user', authRoute)
 
 app.listen(3000, () => {
     console.log(`Server Up and Running..!!`);
